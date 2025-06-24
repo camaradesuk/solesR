43d1430b7c4f058db51b199d291c703e3a838c57
emma-wilson <emma.wilson@ed.ac.uk>
2025-06-24 15:50:38 +0100

    Add additional tests and move test files

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -1,30 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+# Test manual search upload function ===========================================
 
-# 
-# # upload files
-# wos1 <- manual_upload("wos.ris", source = "wos")
-# wos2 <- manual_upload("wos.bib", source = "wos")
-# medline <- manual_upload("medline.xml", source = "xml")
-# scopus <- manual_upload("scopus.ris", source = "scopus") 
-# psychinfo <- manual_upload("psychinfo.ris", source = "psychinfo")
-# embase <- manual_upload("embase.ris", source = "embase")
-# eric <- read_xml("eric.xml", source="xml")
-# 
-# # Load the functions
-# source("your_script_with_functions.R")  # Assuming your functions are in this script
-
-# Define the tests
+# Expect dataframe return on valid input
 test_that("manual_upload() function works as expected", {
   # Define test cases
-  wos1 <- manual_upload("wos.ris", source = "wos")
-  wos2 <- manual_upload("wos.bib", source = "wos")
-  medline <- manual_upload("medline.xml", source = "endnote")
-  scopus <- manual_upload("scopus.ris", source = "scopus") 
-  psychinfo <- manual_upload("psychinfo.ris", source = "psychinfo")
-  embase <- manual_upload("embase.ris", source = "embase")
-  eric <- manual_upload("eric.xml", source="endnote")
+  wos1 <- manual_upload("search_manual/wos.ris", source = "wos")
+  wos2 <- manual_upload("search_manual/wos.bib", source = "wos")
+  medline <- manual_upload("search_manual/medline.xml", source = "endnote")
+  scopus <- manual_upload("search_manual/scopus.ris", source = "scopus") 
+  psychinfo <- manual_upload("search_manual/psychinfo.ris", source = "psychinfo")
+  embase <- manual_upload("search_manual/embase.ris", source = "embase")
+  eric <- manual_upload("search_manual/eric.xml", source="endnote")
   
   # Write assertions for each test case
   expect_true(is.data.frame(wos1), info = "wos1 should be a data frame")
@@ -34,4 +19,14 @@ test_that("manual_upload() function works as expected", {
   expect_true(is.data.frame(psychinfo), info = "psychinfo should be a data frame")
   expect_true(is.data.frame(embase), info = "embase should be a data frame")
   expect_true(is.data.frame(eric), info = "eric should be a data frame")
+})
+
+# Expect error on invalid inputs
+test_that("manual_upload() function handles errors", {
+  # Error on invalid file path
+  expect_error(manual_upload("search_manual/missingfile.bib", source = "wos"))
+  # Error on invalid source
+  expect_error(manual_upload("search_manual/wos.bib", source = "web of science"))
+  # Expect error on invalid file type for source
+  expect_error(manual_upload("seach_manual/wos.bib", source = "endnote"))
 })
