a90acb352eb402f0f54f128b214f0f1a79a0aea7
emma-wilson <emma.wilson@ed.ac.uk>
2025-06-24 15:51:31 +0100

    Move regex test file

--- tests/testthat/test-pico_utils.R
+++ tests/testthat/test-pico_utils.R
@@ -1,6 +1,6 @@
 test_that("clean_regex_file() works as expected", {
   
-  regex_file <- data.table::fread(testthat::test_path("regex_file.csv"))
+  regex_file <- data.table::fread(testthat::test_path("regex/regex_file.csv"))
   req_cols = c("name","regex","type","main_category","sub_category1","sub_category2")
   # input col names must be correct
   expect_contains(colnames(regex_file), req_cols)
