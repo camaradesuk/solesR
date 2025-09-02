e1780a792216d0efcd64709620ada5bc19a3adbe
emma-wilson <emma.wilson@ed.ac.uk>
2025-09-02 16:02:53 +0100

    Add documentation

--- R/search_api.R
+++ R/search_api.R
@@ -5,9 +5,11 @@
 #' Requires `devtools::install_github(kaitlynhair/scopusAPI)`.
 #' Requires an API key from Scopus (http://dev.elsevier.com/).
 #' Use the function `usethis::edit_r_environ()` to add the key to your ~/.Renviron file.
+#' The timespan should be formatted as a number followed by the word "week" or "month", e.g. "1month" or "2week".
 #'
 #' @param query a character string containing a correctly syntaxed Scopus search
 #' @param api_key a working Scopus API key
+#' @param timespan a formatted character string defining the timespan you want to search
 #' @param retMax The maximum number of records to retrieve, default is 2000, maximum is 5000
 #' @param format_soles boolean, if set to TRUE will format search results for the SOLES workflow, default is TRUE
 #' @return a dataframe containing Scopus search results
@@ -21,7 +23,7 @@
 #' @import scopusAPI
 #' @export
 #'
-scopus_search <- function(query = NULL, api_key = NULL, retMax = 2000, format_soles = TRUE) {
+scopus_search <- function(query = NULL, api_key = NULL, timespan = NULL, retMax = 2000, format_soles = TRUE) {
   # Check for query and exit if NULL
   if (is.null(query)) {
     stop(message("Error: you have not entered a search query"))
