b07f1202285462f1795f179f4d4360f62706fbcb
emma-wilson <emma.wilson@ed.ac.uk>
2025-09-30 14:39:16 +0100

    fix error in column name

--- R/get_missing_metadata.R
+++ R/get_missing_metadata.R
@@ -241,12 +241,11 @@ get_missing_dois <- function(citations){
   
   # Try to match based on other metadata
   try(correct_doi <- match %>%
-        select(title, authorships, pages, first_page, last_page, journal, source_display_name, uid, new_doi) %>%
+        select(title, authorships, pages, first_page, last_page, journal, source_display_name, uid, new_doi = doi.y) %>%
         tidyr::unnest(cols=authorships) %>%
         filter(author_position == "first") %>%
         rename(author_orig = author,
-               au_display_name = display_name,
-               new_doi=doi.y) %>%
+               au_display_name = display_name) %>%
         select(title, au_display_name, author_orig, pages, first_page, last_page, journal, source_display_name, uid, new_doi) %>%
         unique()%>%
         filter(!is.na(new_doi)) %>%
