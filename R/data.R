0657b14987f939f35a62697b20e863e8beabbfaa
seancsmith <seancsmith87@gmail.com>
2026-02-25 16:56:06 +0000

    field code documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Field conversion codes for pubmed imports
+#'
+#' @docType data
+#' @author Sean Smith \email{ssmith49@ed.ac.uk}
+"field_codes_pubmed"
+
+#' Field conversion codes for Web of Science imports
+#'
+#' @docType data
+#' @author Sean Smith \email{ssmith49@ed.ac.uk}
+"field_codes_wos"
+
+#' Field conversion codes for scopus imports
+#'
+#' @docType data
+#' @author Sean Smith \email{ssmith49@ed.ac.uk}
+"field_codes_scopus"
